000305ba94475af449e99c8fbd5f6098f7e277a0
Mica <micajuarez26@hotmail.com>
2026-03-17 18:56:37 -0300

    Add backend for cart: CartController and routes

--- shoplite-project/src/app/components/cart.component/cart.component.ts
+++ shoplite-project/src/app/components/cart.component/cart.component.ts
@@ -1,22 +1,22 @@
-import { Component, computed } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>Cart works</p>`,
+  imports: [CommonModule, CurrencyPipe],
+  templateUrl: './cart.component.html',
 })
 export class CartComponent {
+  private router = inject(Router);
+  private cartService = inject(CartService);
 
-  constructor(private cartService: CartService) {}
+  cartItems = this.cartService.cartItems;
+  total = this.cartService.total;
 
-  // signal dal service
-  // cartItems = this.cartService.cartItems;
-
-  // totale calcolato
-  // total = this.cartService.total;
+  isLoading = signal(false);
 
   increase(productId: number) {
     this.cartService.increaseQuantity(productId);
@@ -30,7 +30,25 @@ export class CartComponent {
     this.cartService.removeFromCart(productId);
   }
 
-  checkout() {
-    this.cartService.checkout();
+async checkout() {
+    if (this.cartItems().length === 0) 
+      alert('Il tuo carrello è vuoto!');
+      return;
+
+    const confirmOrder = confirm("Sei sicuro di voler confermare l'ordine?");
+    if (!confirmOrder) return;
+
+    this.isLoading.set(true);
+
+    try {
+      await this.cartService.checkout();
+      alert('Ordine completato con successo! Grazie per il tuo acquisto.');
+      this.router.navigate(['/']); 
+    } catch (error) {
+      console.error('Errore durante il checkout:', error);
+      alert('Si è verificato un errore durante l\'elaborazione dell\'ordine. Per favore, riprova più tardi.');
+    } finally {
+      this.isLoading.set(false);
+    }
   }
 }
