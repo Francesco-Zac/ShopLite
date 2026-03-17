3765ef74f8798921e975340a486c8ad0a18d006a
Mica <micajuarez26@hotmail.com>
2026-03-17 08:08:29 -0300

    feat(cart): initial complete implementation

--- shoplite-project/src/app/app.routes.ts
+++ shoplite-project/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './components/cart.component/cart.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+    path: 'cart',
+    component: CartComponent
+  }
+];
