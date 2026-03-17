b4ae12140a6b957002710ae8f02af4911f25ee3b
Francesco-Zac <francescozaccaro2@gmail.com>
2026-03-17 22:55:31 +0100

    feat(auth + products): admin login, JWT, product CRUD

---
+++ shoplite-project/backend/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'ecommerce_db',
+  port: 3306,
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Errore connessione DB:', err);
+  } else {
+    console.log('Connesso a MySQL!');
+    connection.release();
+  }
+});
+
+module.exports = db;
